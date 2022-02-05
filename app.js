43a0f559c1242bde35be1ba60eb7d7c2a7b21db9
jampoolG <jampoolgarcia@gmail.com>
2022-02-05 12:38:01 -0300

    error corrected, added static folder

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-// Importacion de las librerias a necesarias
-const express = require("espress");
+// Importación de las librerías a necesarias
+const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 
-// configuracion de multer
+// Configuración de multer
 const storage = multer.diskStorage({
-  // cambia el nombre de los archivos para que no se repitan.
+  // Cambia el nombre de los archivos para que no se repitan.
   filename: function(res, file, cb) {
-    // obtiene la extencion del archivo original.
+    // Obtiene la extensión del archivo original.
     const ext = file.originalname.split(".").pop();
-    // genera un numero de milisegundos pasados para poner de nombre.
+    // Genera un numero de milisegundos pasados para poner de nombre.
     const fileName = Date.now();
-    // funcion cb que devuelve el nuevo nombre de el archivo
+    // Función cb que devuelve el nuevo nombre de el archivo
     cb(null, `${fileName}.${ext}`);
   },
-  // maneja el destino en el que se almacenara el archivo
+  // Maneja el destino en el que se almacenara el archivo
   destination: function(res, file, cb) {
     cb(null, `./public`);
   }
 });
 
-
-// Declaracion la variable
+// Declaración la variable
 const app = express();
 const port = process.env.PORT || 3000;
 const upload = multer({storage});
 
+// Configuración de carpeta publica
+app.use(express.static(`./public`));
 
 // Inicia el middleware cors
 app.use(cors());
 
 // rutas
-app.post("upload", upload.single(`myFile`), (req, res)=> {
-  res.send({ ok: `true` })
+app.post("/upload", upload.single(`myFile`), (req, res)=> {
+  const fileName = req.file.filename;
+  res.send({ 
+    ok: `true`,
+    url: `http://localhost:3000/${fileName}` 
+  })
 });
 
 // Inicia la escucha del servidor y envía un mensaje por consola.
